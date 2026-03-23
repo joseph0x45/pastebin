58ddd5e476d269052987d006ac1d61eb23f5d092
joseph0x45 <zozozozeph@gmail.com>
2026-03-23 17:54:50 +0000

    setup project

---
+++ Makefile
@@ -0,0 +1,14 @@
+VERSION := $(shell git describe --tags --abbrev=0)
+APP := pastebin
+
+build: tailwind.css
+	go build .
+
+tailwind.css:
+	curl https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4 > tailwind.css
+
+release: tailwind.css
+	GOOS=linux GOARCH=amd64 \
+		go build -tags release \
+		-ldflags '-X github.com/joseph0x45/pastebin/internal/buildinfo.Version=$(VERSION)' \
+		-o $(APP)
---
+++ internal/cli/cli.go
@@ -0,0 +1,45 @@
+package cli
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/joseph0x45/goutils"
+	"github.com/joseph0x45/pastebin/internal/buildinfo"
+)
+
+func printUsage() {
+	fmt.Printf(`pastebin %s
+
+Usage:
+  pastebin <command> [flags]
+
+Commands:
+  version                        Print the current version
+  help                           Print this help message
+  service-file                   Generate a systemd service file
+
+`, buildinfo.Version)
+}
+
+func DispatchCommands(args []string) {
+	if len(args) == 1 {
+		return
+	}
+	cmd := args[1]
+	switch cmd {
+	case "help":
+		printUsage()
+		os.Exit(0)
+	case "version":
+		fmt.Println(buildinfo.AppName, buildinfo.Version)
+		os.Exit(0)
+	case "service-file":
+		goutils.GenerateServiceFile("<no value>")
+		os.Exit(0)
+	default:
+		fmt.Fprintf(os.Stderr, "Unrecognized command '%s'\n", cmd)
+		printUsage()
+		os.Exit(1)
+	}
+}
